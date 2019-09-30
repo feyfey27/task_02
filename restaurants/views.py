f1c79b48e54b6e8df49c80de2420e57dfcc2205a
Fay Alawadhi <fayalawadhi@Fays-MacBook-Pro.local>
2019-09-30 19:29:29 +0300

    passed

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def task2view(request):
+	context = {
+		"msg": "Hello World!"
+	}	
+	return render(request, 'task2.html', context)
+
